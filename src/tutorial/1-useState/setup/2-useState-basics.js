420a8fb1ea79d23fe11e0329f05a1276b80e405b
Senior <abhishek234543@gmail.com>
2022-07-10 14:01:05 +0530

    use State complete

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
 
+/*
+hooks always start with use
+component name must be uppercase
+must be in the function/component body
+cannot call conditionally
+
+*/
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  /*
+  console.log(useState('hello world'));
+  const value = useState(1)[0];
+  const handler = useState(1)[1];
+  console.log(value, handler);
+  */
+  const [text, setText] = useState('Random title');
+  const handlerClick = () => {
+    if (text === 'Random title') setText('puppie');
+    else setText('Random title');
+
+  };
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className='btn' onClick={handlerClick}>change</button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,30 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const handlerOnClick = () => {
+    if (people.length === 0) return setPeople(data);
+    else return setPeople([]);
+  };
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  }
+  return (
+    <>
+      {people.map((person) => {
+        // console.log(person);
+        const { id, name } = person;
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
+      })}
+      <button className='btn' onClick={handlerOnClick}>clear items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
