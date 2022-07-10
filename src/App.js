063118856583511ae97f98e3515878c642f8683c
Senior <abhishek234543@gmail.com>
2022-07-10 17:25:21 +0530

    useState completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/3-useState-array';
+import Setup from './tutorial/1-useState/setup/5-useState-counter';
 
 function App() {
   return (
     <div className='container'>
       <Setup />
     </div>
-  ) 
+  )
 }
 
 export default App
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState(
+    { id: 1, name: 'john', age: '69', message: 'random message' },
+    { id: 2, name: 'peter', age: '69', message: 'random message' },
+    { id: 3, name: 'susan', age: '69', message: 'random message' },
+    { id: 4, name: 'anna', age: '69', message: 'random message' })
+  console.log(person);
+
+  const [name, setName] = useState('peter');
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState('Random Message');
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hello world' });
+    if (message === 'Random Message' || person === 'Hello World') setMessage('sexxy');
+    else setMessage('Hello World');
+  }
+  return (
+    <>
+      <h3>{person.id}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className='btn' onClick={changeMessage}>Change message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
